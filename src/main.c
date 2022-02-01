6127323baa7ec1f3761f0fee9e56796a295efdef
rafaelrc7 <rafaelrc7@gmail.com>
2022-02-01 03:32:05 -0300

    Added license

--- src/main.c
+++ src/main.c
@@ -1,8 +1,27 @@
-#include <stdio.h>
+/*
+ *	Bintest test runner
+ *	Copyright (C) 2022  Rafael Carvalho
+ *
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
-int main(void)
+#include <argp.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
 {
-	puts("Hello, world!");
 	return 0;
 }
 
